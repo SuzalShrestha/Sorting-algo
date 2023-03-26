66020ec89a9a29eb5fbadc4200b7ae846f01ed6e
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-03-26 15:54:26 +0545

    completed bubble-sort

--- Bubble-sort/bubble-sort.js
+++ Bubble-sort/bubble-sort.js
@@ -1,18 +1,24 @@
-
 function bubbleSort(arr) {
-
-    // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
-        console.log(arr.join(","));
-
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
+  // Iterate through the array
+  let swap = 0;
+  for (let i = 0; i < arr.length - 2; i++) {
+    let current = arr[i];
+    let next = arr[i + 1];
+    // If the current value is greater than its neighbor to the right
+    // Swap those values
+    if (current > next) {
+      let temp = arr[i];
+      arr[i] = next;
+      arr[i + 1] = temp;
+      swap++;
+      // Do not move this console.log
+      console.log(arr.join(","));
+    }
+  }
+  // If you get to the end of the array and no swaps have occurred, return
+  if (swap === 0) return;
+  // Otherwise, repeat from the beginning
+  bubbleSort(arr);
 }
 
 module.exports = bubbleSort;
